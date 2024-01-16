363ad88f9741a4838b37004a6dc73ded412e4184
fireant <societyhater2@gmail.com>
2024-01-16 22:46:37 +0545

    first keyboard interrupt

--- src/kernel.asm
+++ src/kernel.asm
@@ -6,22 +6,35 @@ CODE_SEG equ 0x08
 DATA_SEG equ 0x10 
 
 _start:
-  mov ax, DATA_SEG
-  mov ds, ax
-  mov es, ax
-  mov fs, ax
-  mov gs, ax
-  mov ss, ax
-  mov ebp, 0x00200000
-  mov esp, ebp
+mov ax, DATA_SEG
+mov ds, ax
+mov es, ax
+mov fs, ax
+mov gs, ax
+mov ss, ax
+mov ebp, 0x00200000
+mov esp, ebp
 
-  ; enable a20 line
-  in al, 0x92
-  or al, 2
-  out 0x92, al
+; enable a20 line
+in al, 0x92
+or al, 2
+out 0x92, al
 
-  call kernel_main
+; Remap the master PIC
+mov al, 00010001b
+out 0x20, al; tell the master PIC
 
-  jmp $
+mov al, 0x20 ;interrup 0x20 is the place master ISR is starting
+out 0x21, al
+
+mov al, 00000001b
+out 0x21, al
+;End master pic
+
+sti ;enable interrupts
+
+call kernel_main
+
+jmp $
 
 times 512-($ - $$) db 0
